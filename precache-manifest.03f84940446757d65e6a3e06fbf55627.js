5370d2886de5659ee7b117efac1521be721ef66f
kkasperek <kt.kasperek@gmail.com>
2018-11-19 21:10:12 -0600

    Updates

---
+++ precache-manifest.03f84940446757d65e6a3e06fbf55627.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "55abe30a270b3be75741",
+    "url": "/react-clicky-game/static/js/runtime~main.55abe30a.js"
+  },
+  {
+    "revision": "6fdb98b1cc960bb1dec9",
+    "url": "/react-clicky-game/static/js/main.6fdb98b1.chunk.js"
+  },
+  {
+    "revision": "8fdf2345b7a4e11badcc",
+    "url": "/react-clicky-game/static/js/1.8fdf2345.chunk.js"
+  },
+  {
+    "revision": "6fdb98b1cc960bb1dec9",
+    "url": "/react-clicky-game/static/css/main.990d238b.chunk.css"
+  },
+  {
+    "revision": "de6c5356733948c30cc1eb43a9e366d1",
+    "url": "/react-clicky-game/index.html"
+  }
+];
